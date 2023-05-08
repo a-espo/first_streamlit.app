0d053b0d7b6f4a8b6bc1dd8effc086bfa2463f3e
a-espo <esposita8@newpaltz.edu>
2023-05-08 16:27:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur_exectute("insert into fruit_load_list values ('from streamlit")
+my_cur_execute("insert into fruit_load_list values ('from streamlit")
